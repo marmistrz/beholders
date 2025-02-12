7ae2c11f087c7006f51dad833ba595fd1c4013e3
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2025-02-12 22:21:44 +0100

    KZG init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::time::Instant;
+
+use kzg::{eip_4844::load_trusted_setup_filename_rust, eip_7594::BlstBackend, types::kzg_settings::FsKZGSettings};
+use kzg_traits::{EcBackend, Poly};
+
+const TRUSTED_SETUP_FILE: &str = "trusted_setup.txt";
+
+
+
+struct Prover {
+    trusted_setup: FsKZGSettings,
+}
+
+impl Prover {
+    fn new() -> Result<Self, String> {
+        let trusted_setup = load_trusted_setup_filename_rust(TRUSTED_SETUP_FILE)?;
+        Ok(Self {trusted_setup})
+    }
+
+    fn prove<B: EcBackend>(&self, data: &[i32]) {
+        println!("Hello, world!");
+    }}
+
 fn main() {
-    println!("Hello, world!");
+    let data = [1, 2, 3, 4];
+    let start = Instant::now();
+    let prover = Prover::new().unwrap();
+    let duration = start.elapsed();
+
+    println!("Initialization time: {:?}", duration);
 }
